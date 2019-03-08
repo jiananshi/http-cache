25a37995093d002a1cd34c52af73d4bd90255e4b
jiananshi <klamtine@gmail.com>
2019-03-08 16:16:02 +0800

    $feat: add etag

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
   "net/http"
+  "io"
   "io/ioutil"
   "fmt"
   "log"
   "time"
+  "crypto/md5"
+  "encoding/hex"
 )
 
 func main() {
@@ -22,15 +25,25 @@ func main() {
 
   http.HandleFunc("/test.js", func(w http.ResponseWriter, r *http.Request) {
     data, err := ioutil.ReadFile("test.js")
+
     if err != nil {
       log.Fatal(err)
     }
 
+    h := md5.New()
+    io.WriteString(h, string(data))
+    etag := hex.EncodeToString(h.Sum(nil))
+
     ifModified := r.Header.Get("if-modified-since")
+    ifNoneMatch := r.Header.Get("if-none-match");
+
     w.Header().Set("last-modified", lastModified)
     w.Header().Set("cache-control", "public, max-age=5")
+    w.Header().Set("etag", etag)
 
-    if ifModified == lastModified {
+    if ifNoneMatch == etag {
+      w.WriteHeader(304)
+    } else if ifModified == lastModified {
       lastModified = time.Now().Format(http.TimeFormat)
       w.WriteHeader(304)
     } else {
